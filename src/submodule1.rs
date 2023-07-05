911ff163e8ede96be0ca2b2d51a9dd1d227653df
lsh <1318849690@qq.com>
2023-07-05 12:59:35 +0800

    s

--- src/submodule1.rs
+++ src/submodule1.rs
@@ -1,5 +1,8 @@
 pub fn print_chars() {
-    for c in b'a'..=b'Z' {
+    for c in b'a'..=b'z' {
+        println!("{}", c as char);
+    }
+    for c in b'A'..=b'Z' {
         println!("{}", c as char);
     }
 }
